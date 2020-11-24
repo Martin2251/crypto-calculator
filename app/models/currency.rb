54541c385a05779e42bb9cde98b8896fcc3e4e7c
martin lowe <martinlowe673@gmail.com>
2020-11-24 20:26:21 +0000

    added component files

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,8 +1,13 @@
-class Currency < ApplicationRecord
+class Currency <
+
+  def calculate_value(amount)
+    current_price.to_f * amount.to_f.round(4)
+  end
+
   def current_price
     url = 'https://api.coinmarketcap.com/v1/ticker/'
     request = HTTParty.get(url + self.slug)
-    response = JSON.parse(request.body)
+    response = JSON.parse(request.body)[0]["price-usd"]
 
   end
 end
